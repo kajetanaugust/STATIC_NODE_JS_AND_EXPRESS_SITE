f67bb5ac4fedd686dbcf327862cde110585a9243
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-04-16 17:56:00 +0200

    creating sites

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const { projectsInfo } = require('./data.json');
+const { projects } = require('./data.json');
 
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', {projectsInfo});
+    res.render('index', {projects});
 });
 
 app.get('/about', (req, res) => {
@@ -21,8 +21,8 @@ app.get('/about', (req, res) => {
 app.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
 
-    if (projectId < projectsInfo.length) {
-        const project = projectsInfo.find( ({ id }) => id === +projectId );
+    if (projectId < projects.length) {
+        const project = projects.find( ({ id }) => id === +projectId );
         res.render('project', { project });
     } else {
         res.sendStatus(404);
