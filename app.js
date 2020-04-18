35e38591b3e283ccb8eb092a4ab42339374a9ced
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-04-18 19:24:16 +0200

    finished errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
+app.use(bodyParser.urlencoded({extended:false}));
 const { projects } = require('./data.json');
 
 app.use('/static', express.static('public'));
@@ -14,20 +15,36 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
     const projectId = req.params.id;
 
     if (projectId < projects.length) {
         const project = projects.find( ({ id }) => id === +projectId );
         res.render('project', { project });
-    } else {
-        res.sendStatus(404);
+        next();
+    }
+    else {
+        let err = new Error('Project not found!');
+        err.status =`Error ${404}`;
+        next(err);
     }
 });
 
+app.use((req, res, next) => {
+    const err = new Error('Page not found!')
+    err.status = `Error ${404}`;
 
-//error handling
+    next(err)
+});
+
+
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    console.log(`${err.status} ${err.message}`)
+    console.log(`${err.stack}`)
+    res.render('error')
+});
 
 //exceeds expectations
 
