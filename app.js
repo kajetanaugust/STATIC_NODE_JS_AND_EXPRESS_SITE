b0705bcad0a703cec1f387e14791537299af5b0e
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-04-19 18:14:33 +0200

    finished styling

--- app.js
+++ app.js
@@ -1,53 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const app = express(); //setting app to express
 app.use(bodyParser.urlencoded({extended:false}));
-const { projects } = require('./data.json');
+const { projects } = require('./data.json'); //getting json data
 
-app.use('/static', express.static('public'));
-app.set('view engine', 'pug');
+app.use('/static', express.static('public')); //setting static route
+app.set('view engine', 'pug'); //setting view engine
 
-app.get('/', (req, res) => {
-    res.render('index', {projects});
+app.get('/', (req, res) => { //setting route for index
+    res.render('index', {projects}); //rendering index
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get('/about', (req, res) => { //setting route for about
+    res.render('about'); //rendering about
 });
 
-app.get('/project/:id', (req, res, next) => {
-    const projectId = req.params.id;
+app.get('/project/:id', (req, res, next) => { //setting route for projects
+    const projectId = req.params.id; //getting id from url
 
-    if (projectId < projects.length) {
-        const project = projects.find( ({ id }) => id === +projectId );
-        res.render('project', { project });
-        next();
+    if (projectId < projects.length) { //checking if projectId is lower number than the length of projects array
+        const project = projects.find( ({ id }) => id === +projectId ); //finding the project that matches projectId
+        res.render('project', { project }); //rendering the project
     }
     else {
-        let err = new Error('Project not found!');
-        err.status =`Error ${404}`;
-        next(err);
+        let err = new Error('Project not found!'); //creating new error
+        err.status =`Error ${404}`; //setting error status
+        next(err); //passing err to next middleware
     }
 });
 
-app.use((req, res, next) => {
-    const err = new Error('Page not found!')
-    err.status = `Error ${404}`;
-    next(err)
+app.use((req, res, next) => { //
+    const err = new Error('Page not found!') //creating new error
+    err.status = `Error ${404}`; //setting error status
+    next(err) //passing err to next middleware
 });
 
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-    console.log(`${err.status} ${err.message}`)
-    console.log(`${err.stack}`)
-    res.render('error')
+    res.locals.error = err; //setting error
+    console.log(`${err.status} ${err.message}`) //logging status and message to the console
+    console.log(`${err.stack}`) //logging error stack to the console
+    res.render('error') //rendering error page
 });
 
-//css
 
-//readme
-
-
-app.listen(3000, () => {
-    console.log('The app is running on localhost:3000!')
+app.listen(3000, () => { //setting localhost
+    console.log('The app is running on localhost:3000!') //loging message to the console
 });
